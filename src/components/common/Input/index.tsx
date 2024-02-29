d33df43b6b11da77095bfc113a24645a9283b10c
wwontk <wwontk@gmail.com>
2024-02-29 15:23:17 +0900

    [Feat] 공통 컴포넌트 Input 추가

---
+++ src/components/common/Input/index.tsx
@@ -0,0 +1,30 @@
+interface InputProps {
+  type: "text" | "email" | "password";
+  value: string;
+  width?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({
+  type,
+  value,
+  width,
+  disabled,
+  placeholder,
+  onChange,
+}: InputProps) => {
+  return (
+    <input
+      type={type}
+      value={value}
+      className={`rounded-lg px-3 bg-gray-200 ${width || "w-full"} h-10`}
+      placeholder={placeholder}
+      disabled={disabled}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Input;
