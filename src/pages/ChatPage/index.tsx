7f649da70e7cb69c0a1367309ff0f38d7f4c4015
Pinn <lsol1130@naver.com>
2024-02-29 21:05:44 +0900

    Feat/chatting
    webSocket 사용한 채팅 기능 구현

---
+++ src/pages/ChatPage/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ChatPage = () => (
+  <ul>
+    <li>
+      <Link to="/chat/1">먼지엄마</Link>
+    </li>
+  </ul>
+);
+
+export default ChatPage;
