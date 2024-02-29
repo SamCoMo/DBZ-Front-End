8a6365c3e3fcf543c47aab1e0080c50307bd7e60
wwontk <wwontk@gmail.com>
2024-02-29 15:21:05 +0900

    [Feat] 회원가입 UI 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import InnerCon from './components/common/InnerCon';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import InnerCon from "./components/common/InnerCon";
+import SignupPage from "./pages/SignupPage";
 
 interface AppProps {
   children: React.ReactNode;
@@ -12,11 +13,12 @@ interface AppProps {
 const App = ({ children }: AppProps) => (
   <RecoilRoot>
     <InnerCon>
-      {' '}
+      {" "}
       <Routes>
         <Route>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
           {/* 404 처리 */}
           {/* <Route path="*" element={<NotFound />}></Route> */}
         </Route>
