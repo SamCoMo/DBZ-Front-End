4fd795cb301bf75aaf11cd71d131354b90886404
Pinn <lsol1130@naver.com>
2024-03-02 12:32:59 +0900

    Feat/Design
    공통 컴포넌트 사용 예쩡인 Input/header

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import InnerCon from '@/components/common/InnerCon';
+import React from "react";
+import InnerCon from "@/components/common/InnerCon";
+import Input from "@/components/common/Input";
 
 interface HomePageProps {}
-
-const HomePage = ({}: HomePageProps) => <>홈입니다</>;
+const HomePage = ({}: HomePageProps) => (
+  <InnerCon>
+    홈입니다
+    <Input type="text" />
+  </InnerCon>
+);
 
 export default HomePage;
