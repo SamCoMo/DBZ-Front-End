2cfda69926820ed0085e033341be911fcae79e2d
wwontk <wwontk@gmail.com>
2024-03-09 16:05:53 +0900

    [Design] disabled 디자인 수정

--- src/components/common/Button/WideButton/index.tsx
+++ src/components/common/Button/WideButton/index.tsx
@@ -3,12 +3,14 @@ import React from "react";
 interface WideButtonProps {
   type?: string;
   text: string;
+  disabled?: boolean;
 }
 
-const WideButton = ({ type, text }: WideButtonProps) => (
+const WideButton = ({ type, text, disabled }: WideButtonProps) => (
   <button
     type={type ? "button" : "submit"}
-    className="max-w-default m-auto w-full bg-defaultColor text-white text-body1 h-14 fixed bottom-0 left-0 right-0"
+    disabled={disabled}
+    className={`max-w-default m-auto w-full ${disabled ? "bg-slate-200" : "bg-defaultColor"} text-white text-body1 h-14 fixed bottom-0 left-0 right-0`}
   >
     {text}
   </button>
