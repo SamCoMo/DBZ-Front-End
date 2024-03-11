34301f620f8f09ad9e9f3686ec771632b2c186ad
Pinn <lsol1130@naver.com>
2024-03-12 03:33:31 +0900

    Feat: 이미지 업로드 구현
    - 여러 장의 이미지 배열로 post 가능

--- src/pages/CreateReportPage/index.tsx
+++ src/pages/CreateReportPage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderTitle from "@/components/common/HeaderTitle";
 import Input from "@/components/common/Input";
 import Addr from "@/components/Report/MapAddr";
@@ -7,15 +7,40 @@ import { BsCameraFill } from "react-icons/bs";
 import usePostCreateReportQuery from "@/hooks/query/usePostReportQuery";
 
 const CreateReportPage = () => {
+  const [selectedImages, setSelectedImages] = useState<File[]>([]);
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const images: File[] = Array.from(event.target.files);
+      setSelectedImages(images);
+    }
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // 선택된 이미지를 서버에 업로드하는 코드를 작성합니다.
+    // 이 코드는 서버와의 통신 및 이미지 업로드를 담당합니다.
+  };
   return (
     <>
       <HeaderTitle title="게시글 작성" />
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <p className="mt-3 mb-3">사진</p>
-          <button className="w-32 h-32 border rounded-lg">
-            <BsCameraFill className="text-defaultColor m-auto" />
-          </button>
+          <label htmlFor="images" className="inline-block">
+            <div className="w-32 h-32 border rounded-lg flex items-center justify-center cursor-pointer">
+              <BsCameraFill className="text-defaultColor" />
+            </div>
+            <input
+              className="hidden"
+              type="file"
+              id="images"
+              accept="image/*"
+              multiple
+              onChange={handleImageChange}
+            />
+          </label>
         </div>
         <div>
           <p>제목</p>
