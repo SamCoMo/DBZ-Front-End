f3842fa4046fb569c2a47485ea4d72ca6945879c
Pinn <lsol1130@naver.com>
2024-03-14 02:13:14 +0900

    Fix:라우팅 관련 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import InnerCon from "./components/common/InnerCon";
-import CreateReportPage from "./pages/CreateReportPage";
+import CreateReportPage from "./pages/report/CreateReportPage";
 import SignupPage from "./pages/SignupPage";
+import ReportDetailPage from "./pages/report/ReportDetailPage";
+import ReportEditPage from "./pages/report/ReportEditPage";
 
 interface AppProps {
   children?: React.ReactNode;
@@ -19,7 +21,11 @@ const App = ({ children }: AppProps) => (
         <Route>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/report" element={<CreateReportPage />} />
+          <Route path="/report" element={<Outlet />}>
+            <Route path="create" element={<CreateReportPage />} />
+            <Route path=":id" element={<ReportDetailPage />} />
+            <Route path=":id/edit" element={<ReportEditPage />} />
+          </Route>
           <Route path="/signup" element={<SignupPage />} />
           {/* 404 처리 */}
           {/* <Route path="*" element={<NotFound />}></Route> */}
