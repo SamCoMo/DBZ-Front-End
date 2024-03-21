e18c35e77d42315075949d68ed2f453301eede76
wwontk <wwontk@gmail.com>
2024-03-22 01:11:14 +0900

    [Design] 공백/엔터 수정

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,8 +1,7 @@
-import { postSignUp, getUserProfile, postLogin, postLogout,
-  postWithDraw, } from "./auth/handler";
+import { postSignUp, getUserProfile, postLogin, postLogout, postWithDraw } from "./auth/handler";
 import { getReport, deleteReport, getReportDetail, patchMyReport, postPin, postReport } from "./report/handler";
 import { putProfileImg } from "./user/handler";
 
-const handlers = [postSignUp, postLogin, getUserProfile, putProfileImg, getReportDetail, getReport, postLogout, postWithDraw, postReport,patchMyReport,deleteReport, postPin] ;
+const handlers = [postSignUp, postLogin, getUserProfile, putProfileImg, getReportDetail, getReport, postLogout, postWithDraw, postReport, patchMyReport, deleteReport, postPin] ;
 
 export default handlers;
