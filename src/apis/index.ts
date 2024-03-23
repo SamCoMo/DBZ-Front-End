fa62eaf78c44720be6e4ba8ab08c0d936832ee24
Pinn <lsol1130@naver.com>
2024-03-23 23:22:51 +0900

    fix/배포이슈 해결

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:5173";
+export const BASE_URL = "https://dbz-front-end.vercel.app";
 
 export const axiosDefault = axios.create({
   baseURL: BASE_URL,
