24848044e151d568d3a9ee7d20af5a6b81e3dfc9
Pinn <lsol1130@naver.com>
2024-05-09 00:54:19 +0900

    [Add]채팅 버튼 추가

--- src/pages/report/ReportDetailPage/index.tsx
+++ src/pages/report/ReportDetailPage/index.tsx
@@ -1,16 +1,17 @@
-import React, { useCallback, useState } from 'react';
+import  { useState } from 'react';
 import HeaderTitle from "@/components/common/HeaderTitle";
 import useGetReportDetailQuery from "@/hooks/query/useGetReportQuery";
 import { BsFillPinAngleFill, BsPhoneFill } from "react-icons/bs";
 import ReportDetailKakaoMap from "@/components/common/KakaoMap/ReportDetailMap";
 import { useNavigate, useParams } from "react-router-dom";
 import useUserState from "@/hooks/useUserState";
-import ModalInSelectEdit from "@/components/Report/ModalInSelectEdit";
 import useGetReportPinDetailQuery from "@/hooks/query/useGetReportPinDetailQuery";
 import Modal from "@/components/common/Modal";
 import useModalState from '@/hooks/useModalState';
 import { format } from 'date-fns';
-import usePostChatRoomQuery from '@/hooks/query/usePostChatRoomQuery';
+import CreateChatRoomButton from '@/hooks/query/FirebaseChat/useFirebaseChatQuery';
+// import CreateChatRoomButton from '@/components/common/Button/WideButton/ChattingButton';
+
 
 const ReportDetailPage = () => {
   const navigate = useNavigate();
@@ -26,47 +27,33 @@ const ReportDetailPage = () => {
   const { reportPinDetail, reportPinDetailIsLoading } = useGetReportPinDetailQuery(reportId, selectedPinId);
 
   // 채팅방 생성
-  const { chatRoomCreateIsMutate } = usePostChatRoomQuery();
-
+  const recipientId = reportDetail?.writerProfile.nickname;
+  const currentUserId = userState.nickname;
   const handlePinClick = (pinId: number) => {
     setSelectedPinId(pinId);
     openModal();
-    console.log('Modal should open now');
   };
-
-  const handleCreateChatRoom = useCallback(() => {
-    const recipientId = reportDetail.writerId; // 게시물 생성자 ID를 recipientId로 사용
-  
-    chatRoomCreateIsMutate({
-      recipientId  // recipientId를 payload 객체에 포함
-    }, {
-      onSuccess: (data) => {
-        navigate(`/chat/${data.chatRoomId}`); // 채팅방 생성 성공 후 해당 채팅방으로 이동
-      }
-    });
-  }, [chatRoomCreateIsMutate, navigate, reportDetail]);
 
   if (!reportDetail) {
     return <div>No report detail available.</div>;
   }
-
   const [date, time] = (reportDetail.createdAt ?? '').split('T');
   const  [ pinDate ] = (reportPinDetail?.foundAt ?? '').split('T');
   return (
-    <div>
-      <div className='flex justify-center'>
-        <HeaderTitle title={reportDetail.title} />
-        {userState && userState.memberId === reportDetail.writerId && <ModalInSelectEdit />}
+    <div>      
+      <div>
+        <HeaderTitle title={reportDetail.title} back={true} edit={userState && userState.nickname === reportDetail.writerProfile.nickname}/>
+
       </div>
       <div className="info-section">
         {reportDetail.imageList && reportDetail.imageList.length > 0 && (
           <img src={reportDetail.imageList[0].url} alt="Report Image" className="w-full h-80" />
         )}
         <div className="flex align-middle mx-3 my-3">
-          <img className="w-14 h-14 rounded-full" src={userState.profileImageUrl} alt="Profile" />
+          <img className="w-14 h-14 rounded-full" src={reportDetail.writerProfile.profileImageUrl} alt="Profile" />
           <div className="mx-2">
-            <p>{userState.nickname}</p>
-            <p>📌 {date} {time.slice(0, -9)}</p>
+            <p>{reportDetail.writerProfile.nickname}</p>
+            <p>📌 {date} {time.slice(0, -10)}</p>
           </div>
           <div className="ml-auto">{reportDetail.views}</div>
         </div>
@@ -85,6 +72,7 @@ const ReportDetailPage = () => {
             reportId={reportId}
             onPinSelect={handlePinClick}
           />
+
         {modalState.isOpen && (
           <Modal isOpen={modalState.isOpen} onClose={closeModal}>
             {reportPinDetailIsLoading ? <p>Loading...</p> : (
@@ -101,9 +89,18 @@ const ReportDetailPage = () => {
         )}
         </div>
       </div>
+      <div>
+      <p className="flex justify-start mb-2 mt-4">
+            <BsPhoneFill className="mr-2 text-defaultColor" /> 제보연락: {reportDetail.phone}
+          </p>
+          <p className="flex justify-start mb-2 mt-4">
+          <BsFillPinAngleFill className="mr-2 text-defaultColor" /> {reportDetail.descriptions}            
+          </p>
+      </div>
       <div className="flex justify-evenly my-4">
         <button className="btn bg-defaultColor text-white" onClick={() => navigate(`/report/${reportId}/pin`)}>핀 찍기</button>
-        <button className="btn bg-defaultColor text-white" onClick={handleCreateChatRoom}>채팅하기</button>
+        {/* <CreateChatRoomButton recipientId={recipientId} /> */}
+        <CreateChatRoomButton currentUserId={currentUserId} recipientId={recipientId}/>
       </div>
     </div>
   );
